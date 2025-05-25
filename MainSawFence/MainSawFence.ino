b5e23408913bcc1776590d7f6bc7f4ea2d2d16c4
hjmcphail76 <hjmcphail76@gmail.com>
2025-05-25 19:43:59 -0400

    edited the GUI

--- MainSawFence/MainSawFence.ino
+++ MainSawFence/MainSawFence.ino
@@ -1,29 +1,8 @@
- /*
- * Title: MovePositionAbsolute
- *
- * Requirements:
- * 1. A ClearPath motor must be connected to Connector M-0.
- * 2. The connected ClearPath motor must be configured through the MSP software
- *    for Step and Direction mode (In MSP select Mode>>Step and Direction).
- * 3. The ClearPath motor must be set to use the HLFB mode "ASG-Position
- *    w/Measured Torque" with a PWM carrier frequency of 482 Hz through the MSP
- *    software (select Advanced>>High Level Feedback [Mode]... then choose
- *    "ASG-Position w/Measured Torque" from the dropdown, make sure that 482 Hz
- *    is selected in the "PWM Carrier Frequency" dropdown, and hit the OK
- *    button).
- * 4. Set the Input Format in MSP for "Step + Direction".
- *
- * ** Note: Homing is optional, and not required in this operational mode or in
- *    this example. This example makes positive absolute position moves,
- *    assuming any homing move occurs in the negative direction.
- *
- * ** Note: Set the Input Resolution in MSP the same as your motor's Positioning
- *    Resolution spec if you'd like the pulses sent by ClearCore to command a
- *    move of the same number of Encoder Counts, a 1:1 ratio.
-*/
 #include "ClearCore.h"
 
 //Program inputs:
+
+
 
 // Specifies which motor port you have connected.
 // Options are: ConnectorM0, ConnectorM1, ConnectorM2, or ConnectorM3.
